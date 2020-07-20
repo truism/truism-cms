a9ddb9467cb2448bf9c78681419ced54957b66f1
Admin <liumingli95@gmail.com>
2020-07-21 00:51:07 +0800

    Initial commit

---
+++ src/main/java/site/liuming/truismcms/TruismCmsApplication.java
@@ -0,0 +1,13 @@
+package site.liuming.truismcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruismCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruismCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/liuming/truismcms/TruismCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.liuming.truismcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruismCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
