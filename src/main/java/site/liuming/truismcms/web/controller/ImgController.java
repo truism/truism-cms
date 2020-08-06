843bc982a1ab6e6dd51559c5499c8829f521a6cd
lee.liuming.li <lee.liuming.li@netis.com>
2020-08-06 16:03:50 +0800

    * 准备实现博客图片上传下载功能

---
+++ src/main/java/site/liuming/truismcms/web/controller/ImgController.java
@@ -0,0 +1,13 @@
+package site.liuming.truismcms.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 图片上传下载控制器
+ */
+@RequestMapping("/img")
+public class ImgController {
+
+
+
+}
